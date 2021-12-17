b7f63e95b56338d7f88e6c56046012e9fb17c735
ItsWilsonP <95710544+ItsWilsonP@users.noreply.github.com>
2021-12-17 16:16:30 -0400

    Update 100metres.py

--- 100metres.py
+++ 100metres.py
@@ -1,11 +1,25 @@
 #100 METERS 
 
 import random
+def nicedice(num):
+    if num == 1:
+        return(u"\u2680")
+    elif num == 2:
+        return(u"\u2681")
+    elif num == 3:
+        return(u"\u2682")
+    elif num == 4:
+        return(u"\u2683")
+    elif num == 5:
+        return(u"\u2684")
+    elif num == 6:
+        return(u"\u2685")
+
 total100 = 0 #sum of dice for first four rolls
 attempts100 = 1 # attempts for first half 
 total101 = 0 # sum of dice for second four rolls
-attempts101 = 1  # attempts for second half
 dice100 = [0, 0, 0, 0] #these dice will be used for the entire 100meters program
+nicedice100 = [0, 0, 0, 0] #the nice dice 100meters
 FINALSCORE100 = 0
 for d in range(4): #0,1,2,3
         dice100[0] = random.randint(1,6)
@@ -13,6 +27,8 @@ for d in range(4): #0,1,2,3
         dice100[2] = random.randint(1,6)
         dice100[3] = random.randint(1,6)
         print(dice100)
+        nicedice100 = [nicedice(dice100[0]), nicedice(dice100[1]), nicedice(dice100[2]), nicedice(dice100[3])]
+        print(nicedice100)
         
         total100 = dice100[0] + dice100[1] + dice100[3] + dice100[2]
         if dice100[0] == 6:
@@ -23,13 +39,15 @@ for d in range(4): #0,1,2,3
             total100 = total100 - 6
         if dice100[3] == 6:
             total100 = total100 - 6
-        print(total100)
+        print("you rolled" , total100)
         
-        if attempts100 == 4:
-            print("this was your fourth attempt, you must keep this sum")
+
+        
+        if attempts100 == 7:
+            print("this was your seventh attempt, you must keep this sum")
             break
         else:
-            print("this was your (", attempts100 , ") attempt, you only have four attempts")
+            print("this was your (", attempts100 , ") attempt, you only have seven attempts")
             happy = input("do you want to roll again(y) or keep this sum(n)? (y/n): ")
             if happy == "n" or happy == "n":
                 break
@@ -57,16 +75,16 @@ for d in range(4):
         total101 = total101 - 6
 
     print(total101)
-    if attempts101 == 4:
-        print("this was your fourth attempt, you must keep this sum")
+    if attempts100 == 7:
+        print("this was your seventh attempt, you must keep this sum")
         break
     else:
-        print("this was your (", attempts101 , ") attempt, you only have four attempts")
+        print("this was your (", attempts100 , ") attempt, you only have seven attempts")
         happy = input("do you want to roll again(y) or keep this sum(n)? (y/n): ")
         if happy == "n" or happy == "n":
             break
         elif happy == "y" or happy == "Y":
-            attempts101 = attempts101 + 1
+            attempts100 = attempts100 + 1
         
 print("you kept the a total of:" , total101)
 print("your older score was:", total100)
@@ -74,6 +92,5 @@ print("you final score should be the sum of the two totals") #just a few print s
 FINALSCORE100 = total101 + total100
 print("YOU FINISHED WITH A FINAL SCORE OF", FINALSCORE100) #finalscore
 
-#this is a working program, but could be cleaned up a bit, 
-
-    
+#im happy how the program is running, but i left in the actually numbers and included the nice 
+#dice because i found the nice dice hard to read.
